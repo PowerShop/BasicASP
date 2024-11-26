6ccf439ac86d0fcb9604a9ffe86fb06f52e5d7c6
Kittisak Sakmeang <aaaxcvg@gmail.com>
2024-11-26 17:05:19 +0700

    Add project files.

---
+++ BasicASP/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return Content("คะแนนสอบวิชาคอมพิวเตอร์");
+        }
+
+        public IActionResult Detail()
+        {
+            //return Detail();
+            return Content("รายละเอียดคะแนนสอบวิขาคอมพิวเตอร์");
+        }
+
+        public IActionResult ShowScore(int id)
+        {
+            //return ShowScore();
+            return Content($"คะแนนสอบวิขาคอมพิวเตอร์ รหัส {id}");
+        }
+
+        public IActionResult Error()
+        {
+            // return Error();
+            return Content("Error");
+        }
+    }
+}
